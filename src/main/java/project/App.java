2eb7a0f00fd4c8e674eedaed9db2248dcc6cefed
Max <12usa34airbornE@gmail.com>
2019-10-10 17:31:06 +0300

    Sort array

--- src/main/java/project/App.java
+++ src/main/java/project/App.java
@@ -4,10 +4,16 @@ package project;
  * Hello world!
  *
  */
+import java.util.Arrays;
 public class App 
 {
+    private static int arr1 [] = {11,3,4,5,6,10,29,0};
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Arrays.sort(arr1);
+        for(int i=0; i<arr1.length;i++)
+        {
+            System.out.println( "Hello World!"+arr1[i]);
+        }
     }
 }
